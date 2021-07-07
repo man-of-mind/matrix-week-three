1b412895089ad8a4bd560601a346627464fb5b10
timothy <timothyabiodun300@gmail.com>
2021-07-07 13:51:01 +0100

    made all correctins

--- src/email_parser.py
+++ src/email_parser.py
@@ -4,36 +4,23 @@ import re
 class EmailParser:
     def __init__(self):
         # regex pattern that validate email
-        self.pattern = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+#        self.pattern = r'\b[A-Za-z][A-Za-z0-9+]+@[A-Za-z][A-Za-z0-9]+\.com$'
+        self.pattern = r'^[A-Za-z][A-Za-z0-9+]+@[A-Za-z][A-Za-z0-9]+\.com$'
         self.keys = ['username', 'domain']
 
 
     def convert_to_dict(self, keys, email_list):
         '''
           Takes in two lists as params and return them as a dictionary.'''
-        result = dict(zip(keys, email_list))
-        print(result)
-
+        return dict(zip(keys, email_list))
 
     def parse(self, email):
         ''' parse email and return a dictionary of username and domain if valid and return None otherwise'''
         # Add implementation here ...
-        numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-        check = re.match(self.pattern, email)
+        check = re.search(self.pattern, email)
         if check:
             email_as_a_list = email.split("@")
-            if email_as_a_list[0][0] in numbers or email_as_a_list[1][0] in numbers:
-                print(None)
-            else:
-                self.convert_to_dict(self.keys, email_as_a_list)        
+            return self.convert_to_dict(self.keys, email_as_a_list)        
         else:
-            print(None)
+            return None
 
-
-    
-email = EmailParser()
-mail = ' timothy-abiodun@gmail.com'
-#mail = input('Enter your email address: ')
-#remove trailing and leading whitespaces
-#stripped_mail = mail.strip()
-email.parse(mail)
--- tests/test_email_parser.py
+++ tests/test_email_parser.py
@@ -17,19 +17,28 @@ class TestEmailParser(unittest.TestCase):
         self.assertEqual(self.test_case.parse('3timothyabiodun@gmail.com'), None)
     
     def test_third_parse(self):
-        self.assertEqual(self.test_case.parse('timothy-abiodun@gmail.com'), {'domain': 'gmail.com', 'username': 'timothy-abiodun'})
+        self.assertEqual(self.test_case.parse('timothy-abiodun@gmail.com'), None)
     
     def test_fourth_parse(self):
         self.assertEqual(self.test_case.parse('timothy400@yahoo.com'), {'username': 'timothy400', 'domain': 'yahoo.com'})
 
     def test_fifth_parse(self):
-        self.assertEqual(self.test_case.parse('timothyabiodun3@hotmail.com'), {'username': 'timothyabiodun3', 'domain': 'hotmail'})
+        self.assertEqual(self.test_case.parse('timothyabiodun3@hotmail.com'), {'username': 'timothyabiodun3', 'domain': 'hotmail.com'})
 
     def test_sixth_parse(self):
         self.assertEqual(self.test_case.parse('#timothyabiodun@gmail.com'), None)
 
     def test_seventh_parse(self):
         self.assertEqual(self.test_case.parse(' timothyabiodun@yahoo.com'), None)
+
+    def test_eighth_parse(self):
+        self.assertEqual(self.test_case.parse('timothyabiodun@gmail.2com'), None)
+
+    def test_ninth_parse(self):
+        self.assertEqual(self.test_case.parse('timothyabiodun@yahoo.com2'), None)
+
+    def test_tenth_parse(self):
+        self.assertEqual(self.test_case.parse('jane+doe@gmail.com'), {'username': 'jane+doe', 'domain': 'gmail.com'})
 
 
 
